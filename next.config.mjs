e28db6a7f7f35a9c14655500c61f6d7cad2303bb
RAJGOPAL HOTA <110685523+rajgopalhota@users.noreply.github.com>
2024-10-10 23:02:43 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import nextPWA from 'next-pwa';
-
-// Export the Next.js configuration with PWA support
 /** @type {import('next').NextConfig} */
-const nextConfig = nextPWA({
-  dest: 'public', // Specify where to generate the service worker and related files
-  register: true, // Automatically registers the service worker
-  skipWaiting: true, // Instructs the service worker to take control of the page as soon as it's installed
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-});
+const nextConfig = {};
 
 export default nextConfig;
